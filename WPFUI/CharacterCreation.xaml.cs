6698bde72a5e486a409a1a001d2a8a8b54088084
Scott Lilly <scottlilly@scottlilly.com>
2020-11-01 15:03:37 -0600

    Creating initial commit

---
+++ WPFUI/CharacterCreation.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Engine.ViewModels;
+
+namespace WPFUI
+{
+    public partial class CharacterCreation : Window
+    {
+        private CharacterCreationViewModel VM { get; set; }
+
+        public CharacterCreation()
+        {
+            InitializeComponent();
+
+            VM = new CharacterCreationViewModel();
+            DataContext = VM;
+        }
+
+        private void RandomPlayer_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void UseThisPlayer_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
