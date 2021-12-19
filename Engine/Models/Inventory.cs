a94c6e98bf5ad145fc5902708b9ba461f58ed6c0
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2021-12-19 09:52:40 -0600

    Lesson 19.12: Decouple InventoryService from LivingEntity

--- Engine/Models/Inventory.cs
+++ Engine/Models/Inventory.cs
@@ -64,6 +64,55 @@ namespace Engine.Models
             return items.All(item => Items.Count(i => i.ItemTypeID == item.ItemID) >= item.Quantity);
         }
 
+        public Inventory AddItem(GameItem item)
+        {
+            return AddItems(new List<GameItem> { item });
+        }
+
+        public Inventory AddItems(IEnumerable<GameItem> items)
+        {
+            return new Inventory(Items.Concat(items));
+        }
+
+        public Inventory RemoveItem(GameItem item)
+        {
+            return RemoveItems(new List<GameItem> { item });
+        }
+
+        public Inventory RemoveItems(IEnumerable<GameItem> items)
+        {
+            // REFACTOR: Look for a cleaner solution, with fewer temporary variables.
+            List<GameItem> workingInventory = Items.ToList();
+            IEnumerable<GameItem> itemsToRemove = items.ToList();
+
+            foreach (GameItem item in itemsToRemove)
+            {
+                workingInventory.Remove(item);
+            }
+
+            return new Inventory(workingInventory);
+        }
+
+        public Inventory RemoveItems(IEnumerable<ItemQuantity> itemQuantities)
+        {
+            // REFACTOR
+            Inventory workingInventory = new Inventory(Items);
+
+            foreach (ItemQuantity itemQuantity in itemQuantities)
+            {
+                for (int i = 0; i < itemQuantity.Quantity; i++)
+                {
+                    workingInventory =
+                        workingInventory
+                            .RemoveItem(workingInventory
+                                .Items
+                                .First(item => item.ItemTypeID == itemQuantity.ItemID));
+                }
+            }
+
+            return workingInventory;
+        }
+
         #endregion
 
         #region Private functions
--- Engine/Models/LivingEntity.cs
+++ Engine/Models/LivingEntity.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using Engine.Services;
 using Newtonsoft.Json;
 
 namespace Engine.Models
